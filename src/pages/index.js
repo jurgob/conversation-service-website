799a4dc98e7d40719ea68833b38c453227e0b56d
Jurgo Boemo <jurgo.boemo@gmail.com>
2020-07-29 16:22:20 +0100

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,47 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <IntroPage />
+    </div>
+  )
+}
+//https://www.agora.io/en/
+//https://dashboard.nexmo.com/conversation
+
+
+function IntroPage(){
+  return (<div>
+    <h1>Vonage Conversation API</h1>
+    <p>“Comunicate with your customer real - time.In every possible channel”</p>
+    <p></p>The most flexible comunication api in the world.
+    Comunication is the key for the success of your business.But today comunication can happen in every possible way.With Conversation API you can manage with just one api Telephone Calls, Video Call, Whatsapp, viber, facebook messages, push notification, In App Message and Audio, Mails.
+    Keep the comunication context.Keep track of every comunication.In Conversation API all your comunication datas are stored allowing you to use them in a flexible way.Give your customer a conversation history.Keep track or their past.Make them easy jump from one channel to another.
+    Advance funtionality.Conversation API is far more then this.Record your calls.use text to speech.Take advandtage of the speech to text.Transcribe every audio in a different text.    
+    <p>
+      Functionality of conversations:
+      program telephone calls
+      in app call
+      in app chat
+      in app video
+      customize your push notification
+      Call  Whisper
+      text to speech
+      speech to text
+      audio record
+      sip trunk
+      send/receive messanger api
+      send/receive whatup message
+    </p>
+    <p>
+      Create it with Conversation API
+      Create your own slack, with audio and chat
+      Build your own twitch with our video solution
+      Do your own call Center (mix ivr and human agents)
+      Create your real time chess app
+      create your social network
+
+    </p>
+  </div>)
 }
